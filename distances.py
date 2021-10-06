cbe4af21f255ff3a34406e3d22990d296ae25d9f
Medric Sonwa <medric49@gmail.com>
2021-10-05 22:37:10 -0400

    Add methodify selection processes

--- distances.py
+++ distances.py
@@ -11,28 +11,51 @@ class Distance:
     def distance(self, w1, w2):
         raise NotImplementedError()
 
+    def _select_1(self, w):
+        props = deque([], maxlen=self.max_proposition)
+        min_dist = np.inf
+        for word in self.glossary.keys():
+            dist = self.distance(w, word)
+            if dist <= min_dist:
+                props.appendleft(word)
+        return list(props)
+
+    def _select_2(self, w):
+        props = deque([], maxlen=self.max_proposition)
+        dists = deque([], maxlen=self.max_proposition)
+
+        for word in self.glossary.keys():
+            dist = self.distance(w, word)
+            min_dist = np.inf if len(dists) == 0 else dists[0]
+            if dist <= min_dist:
+                props.appendleft(word)
+                dists.appendleft(dist)
+            elif dist <= np.mean(dists):
+                props.insert(1, word)
+                dists.insert(1, dist)
+        return list(props)
+
+    def _select_3(self, w):
+        props = {}
+        chosen_props = []
+        for word in self.glossary.keys():
+            dist = self.distance(w, word)
+            props[word] = dist
+
+        for i in range(self.max_proposition):
+            word = list(props.keys())[np.argmax(props.values())]
+            chosen_props.append(word)
+            props.pop(word)
+        return chosen_props
+
     def propositions(self, w: str or list):
         if isinstance(w, str):
             if w in self.glossary:
                 return [w]
             else:
-                props = {}
-                chosen_props = []
-                for o_word in self.glossary.keys():
-                    dist = self.distance(w, o_word)
-                    props[o_word] = dist
-
-                for i in range(self.max_proposition):
-                    o_word = list(props.keys())[np.argmax(props.values())]
-                    chosen_props.append(o_word)
-                    props.pop(o_word)
-
-                return chosen_props
+                return self._select_3(w)
         elif isinstance(w, list):
             return {w0: self.propositions(w0) for w0 in w}
-
-
-
 
 
 class NullDistance(Distance):
